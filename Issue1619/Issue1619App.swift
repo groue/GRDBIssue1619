4d5df41c0b3d686775f6c2993edc35f33b96f162
Gwendal Roué <gr@pierlis.com>
2024-09-14 12:16:55 +0200

    Use GRDB, IPHONEOS_DEPLOYMENT_TARGET = 17.6

--- Issue1619/Issue1619App.swift
+++ Issue1619/Issue1619App.swift
@@ -1,17 +1,20 @@
-//
-//  Issue1619App.swift
-//  Issue1619
-//
-//  Created by Gwendal Roué on 14/09/2024.
-//
-
+import GRDB
 import SwiftUI
 
 @main
 struct Issue1619App: App {
+    let sqliteVersion: String = {
+        let writer: any DatabaseWriter = try! DatabaseQueue()
+        return try! writer.read { db in
+            try String.fetchOne(db, sql: "SELECT sqlite_version()")!
+        }
+    }()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            VStack {
+                Text("SQLite version: \(sqliteVersion)")
+            }
         }
     }
 }
