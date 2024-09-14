2ebd9547aab381d5ddac6d359864717e563c0009
Gwendal Roué <gr@pierlis.com>
2024-09-14 12:12:37 +0200

    Initial Commit

---
+++ Issue1619/Issue1619App.swift
@@ -0,0 +1,17 @@
+//
+//  Issue1619App.swift
+//  Issue1619
+//
+//  Created by Gwendal Roué on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Issue1619App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
